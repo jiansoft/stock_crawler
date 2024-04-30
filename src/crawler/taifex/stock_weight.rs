b82fc9fb168d0286d11299c06f612f205c7f3ab5
jIAn <eddiea.chen@gmail.com>
2024-04-30 10:40:34 +0800

    1.調整抓取 TWSE 權重 的selector

--- src/crawler/taifex/stock_weight.rs
+++ src/crawler/taifex/stock_weight.rs
@@ -17,34 +17,64 @@ pub struct StockWeight {
     pub weight: Decimal,
 }
 
+struct ExchangeConfig {
+    url: String,
+    selector: String,
+}
+
+impl ExchangeConfig {
+    /// 建立一個新的交易所組態實例。
+    ///
+    /// # 參數
+    ///
+    /// * `exchange`: 一個 `StockExchange` 列舉，代表選擇的股票交易所。
+    ///
+    /// 返回: 返回一個新的 `ExchangeConfig` 實例，其中包含了訪問特定交易所資料所需的URL和選擇器。
+    ///
+    /// # 示例
+    ///
+    /// ```rust
+    /// let config = ExchangeConfig::new(StockExchange::TWSE);
+    /// println!("URL: {}", config.url);
+    /// println!("Selector: {}", config.selector);
+    /// ```
+    ///
+    /// 上面的程式碼展示了如何建立一個針對台灣證券交易所 (TWSE) 的組態實例，並列印相關的 URL 和選擇器。
+    fn new(exchange: StockExchange) -> Self {
+        match exchange {
+            StockExchange::TWSE => Self {
+                url: format!("https://{}/cht/9/futuresQADetail", taifex::HOST),
+                selector: "#printhere > div > div > table > tbody > tr:not(:first-child)"
+                    .to_string(),
+            },
+            StockExchange::TPEx => Self {
+                url: format!("https://{}/cht/2/tPEXPropertion", taifex::HOST),
+                selector: "#printhere > div > table > tbody > tr:not(:first-child)".to_string(),
+            },
+            _ => panic!("Unsupported exchange"),
+        }
+    }
+}
+
 /// 台股各股權重
 pub async fn visit(exchange: StockExchange) -> Result<Vec<StockWeight>> {
     let mut result: Vec<StockWeight> = Vec::with_capacity(1024);
-    let url = match exchange {
-        StockExchange::TWSE => {
-            format!("https://{}/cht/9/futuresQADetail", taifex::HOST)
-        }
-        StockExchange::TPEx => {
-            format!("https://{}/cht/2/tPEXPropertion", taifex::HOST)
-        }
-        _ => return Ok(result),
-    };
+    let exchange_market = ExchangeConfig::new(exchange);
+    let text = util::http::get(&exchange_market.url, None).await?;
 
-    let text = util::http::get(&url, None).await?;
     if text.is_empty() {
         return Ok(result);
     }
 
     let document = Html::parse_document(text.as_str());
-    let selector = match Selector::parse("#printhere > div > table > tbody > tr:not(:first-child)")
-    {
+    let selector = match Selector::parse(&exchange_market.selector) {
         Ok(selector) => selector,
         Err(why) => {
             return Err(anyhow!("Failed to Selector::parse because: {:?}", why));
         }
     };
 
-    for element in document.select(&selector) {
+    document.select(&selector).for_each(|element| {
         if let Some(sw) = get_stock_weight(
             &element,
             "td:nth-child(1)",
@@ -61,18 +91,32 @@ pub async fn visit(exchange: StockExchange) -> Result<Vec<StockWeight>> {
         ) {
             result.push(sw);
         }
-    }
+    });
 
     Ok(result)
 }
 
-fn get_stock_weight(element: &ElementRef, ra: &str, sy: &str, w: &str) -> Option<StockWeight> {
-    let stock_symbol = element::parse_to_string(element, sy);
-    let weight = element::parse_to_decimal(element, w);
+/// Parses stock weight from an HTML element.
+///
+/// # Arguments
+/// * `element` - A reference to the element to parse from.
+/// * `rank_selector` - CSS selector to find the rank.
+/// * `symbol_selector` - CSS selector to find the stock symbol.
+/// * `weight_selector` - CSS selector to find the weight.
+///
+/// # Returns `Some(StockWeight)` if parsing succeeds, otherwise `None`.
+fn get_stock_weight(
+    element: &ElementRef,
+    rank_selector: &str,
+    symbol_selector: &str,
+    weight_selector: &str,
+) -> Option<StockWeight> {
+    let stock_symbol = element::parse_to_string(element, symbol_selector);
+    let weight = element::parse_to_decimal(element, weight_selector);
 
     if !stock_symbol.is_empty() && !weight.is_zero() {
         let sw = StockWeight {
-            rank: element::parse_to_i32(element, ra),
+            rank: element::parse_to_i32(element, rank_selector),
             stock_symbol,
             weight,
         };
@@ -95,7 +139,7 @@ mod tests {
         dotenv::dotenv().ok();
         logging::debug_file_async("開始 visit".to_string());
 
-        match visit(StockExchange::TPEx).await {
+        match visit(StockExchange::TWSE).await {
             Ok(e) => {
                 dbg!(&e);
                 logging::debug_file_async(format!("len:{}\r\n {:#?}", e.len(), e));
