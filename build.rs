de47f2ec3aedf35c480d324d66562029f12a2f8a
jIAn <eddiea.chen@gmail.com>
2026-06-11 23:45:02 +0800

    Use prost config for vendored protoc

--- build.rs
+++ build.rs
@@ -20,11 +20,8 @@ static OUT_DIR: &str = "src/interfaces/rpc";
 /// 編譯 protobuf 並輸出 Rust 原始碼。
 fn main() -> Result<(), Box<dyn Error>> {
     let protoc = protoc_bin_vendored::protoc_bin_path()?;
-    // SAFETY: build script 在 Cargo 編譯流程中以單一程序執行，且此處會在呼叫
-    // tonic-prost-build 前立即設定 `PROTOC`，沒有其他執行緒同時讀寫環境變數。
-    unsafe {
-        std::env::set_var("PROTOC", protoc);
-    }
+    let mut prost_config = tonic_prost_build::Config::new();
+    prost_config.protoc_executable(protoc);
 
     let protos = [
         "./etc/proto/basic.proto",
@@ -37,7 +34,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     tonic_prost_build::configure()
         .build_server(true)
         .out_dir(OUT_DIR)
-        .compile_protos(&protos, &["./etc/proto"])?;
+        .compile_with_config(prost_config, &protos, &["./etc/proto"])?;
 
     rerun(&protos);
 
